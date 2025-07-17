8a631f0438e6faba28e2c50ef60c239da6784dc3
Maria Olekheyko <maria.olekheyko@microsoft.com>
2025-07-17 17:50:56 +0000

    Edited Spark

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,13 +1,10 @@
-import { useTheme } from "next-themes"
-import { CSSProperties } from "react"
+import { useTheme } from "next-themes
 import { Toaster as Sonner, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+        {
       className="toaster group"
       style={
         {
@@ -15,10 +12,10 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
         } as CSSProperties
-      }
-      {...props}
-    />
-  )
-}
 
-export { Toaster }
+      {...props}
+
+  )
+
+
+
